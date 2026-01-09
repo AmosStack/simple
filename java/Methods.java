4a7601b18ff65d16fe8ce8990f555a533e14028d
Amos M. Abdallah <132259530+Amos-codes-cloud@users.noreply.github.com>
2026-01-09 17:19:39 +0300

    simple java method to add two numbers

---
+++ java/Methods.java
@@ -0,0 +1,11 @@
+public class Method {
+
+    public static int x(int a, int b, int c){
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        int result = x(10, 20, 30);
+        System.out.println("The sum is " + result);
+    }
+}
+ 
