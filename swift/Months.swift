d553d83965124ed8040c5cd5730bafaa942d0f9d
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-13 03:06:25 +0300

    prac

---
+++ swift/Months.swift
@@ -0,0 +1,33 @@
+let month = 6
+let monthName: String
+
+switch month {
+case 1:
+    monthName = "January"
+case 2:
+    monthName = "February"
+case 3:
+    monthName = "March"
+case 4:
+    monthName = "April"
+case 5:
+    monthName = "May"
+case 6:
+    monthName = "June"
+case 7:
+    monthName = "Jully"
+case 8:
+    monthName = "August"
+case 9:
+    monthName = "September"
+case 10:
+    monthName = "October"
+case 11:
+    monthName = "November"
+case 12:
+    monthName = "December"
+default:
+    monthName = "Invalid month"
+}
+
+print("The month is: \(monthName)")
