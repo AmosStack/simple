d1de7431ba75fe7167b53cf3073dd8609aed5db1
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-16 01:30:35 +0300

    Create ArrayElementsPrinter.java

---
+++ java/ArrayElementsPrinter.java
@@ -0,0 +1,11 @@
+// Prints all elements of a predefined integer array.
+public class ArrayElementsPrinter {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9};
+
+        for(int i=0; i < numbers.length; i++ ){
+            System.out.println("ellement at index " + i + " is " + numbers[i]);
+        }
+    
+    }
+}
