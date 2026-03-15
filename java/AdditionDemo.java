653ea53953c19dbf918747f30f18974f15bf74fe
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-16 01:30:06 +0300

    Create AdditionDemo.java

---
+++ java/AdditionDemo.java
@@ -0,0 +1,11 @@
+// Demonstrates a reusable method for adding two integers.
+public class AdditionDemo {
+    public static int sum(int a,int b){
+        return a+b;
+
+    }
+    
+  public static void main(String[]args) {
+     System.out.println(sum (4,6));
+  }
+}
