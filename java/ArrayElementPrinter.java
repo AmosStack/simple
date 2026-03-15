478b14276b4cfe24afe993a9b631f23248f83c66
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-16 01:30:26 +0300

    Create ArrayElementPrinter.java

---
+++ java/ArrayElementPrinter.java
@@ -0,0 +1,10 @@
+// Prints each array element with its index.
+public class ArrayElementPrinter {
+    public static void main (String[] args){
+        int[] numbers ={1,2,3,4,5};
+        for(int i=0; i < numbers.length; i++){   
+            System.out.println("Element at index " + i + " is " + numbers[i]);
+        }
+    }
+     
+} 
