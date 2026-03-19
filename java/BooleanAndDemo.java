4df95f7d3dac05a724d6ddd31276162c87939f2d
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-20 01:19:35 +0300

    Create BooleanAndDemo.java

---
+++ java/BooleanAndDemo.java
@@ -0,0 +1,8 @@
+// Demonstrates boolean AND operation output.
+public class BooleanAndDemo {
+    public static void main(String[] args){
+        boolean a = false;
+        boolean b = true;
+        System.out.println(a && b);
+    }
+}
