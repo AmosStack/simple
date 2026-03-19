367695c90ee1d2c081acad385030bd1094960b77
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-20 01:20:39 +0300

    Create Substring.java

---
+++ java/Strings/Substring.java
@@ -0,0 +1,23 @@
+// Builds a substring manually to compare with Java's built-in substring method.
+public class Substring {
+
+    public static String subString(String str, int st, int ei){
+        String subString = "";
+        for (int i = st; i < ei; i++) {
+            subString+=str.charAt(i);
+        }
+        return subString;
+    }
+    public static void main(String[] args) {
+        String str = "Hello Rajkishor";
+
+        System.out.println(subString(str, 3, 9));
+        System.out.println(str.substring(3,9));
+    }
+}
+
+/*
+ *   Output:
+ *   lo Raj
+ *   lo Raj
+ */
