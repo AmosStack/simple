d7c38862c3b230a45cd39eff06185f0560ad355a
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-20 01:20:04 +0300

    Create GreetingMethodDemo.java

---
+++ java/GreetingMethodDemo.java
@@ -0,0 +1,9 @@
+// Demonstrates calling a static greeting method.
+public class GreetingMethodDemo {
+    public static void greet(){
+        System.out.println("Hello, world! ");
+    }
+    public static void main(String[] args) {
+        GreetingMethodDemo.greet();
+    }
+}
