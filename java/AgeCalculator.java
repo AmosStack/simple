33f64cdf312b905f15e49d4584f1a0efdf25febc
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-20 01:19:04 +0300

    Create AgeCalculator.java

---
+++ java/AgeCalculator.java
@@ -0,0 +1,9 @@
+public class AgeCalculator {
+    public static void main(String[] args) {
+        double sqrtAge = 5;
+
+        double age = Math.pow(sqrtAge, 2);
+
+        System.out.println("Actual age is: " + (int) age);
+    }
+}
