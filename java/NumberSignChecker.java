58d9a8dfab555d5b6dfbf63e1279798737e4bc89
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-20 01:20:32 +0300

    Create NumberSignChecker.java

---
+++ java/NumberSignChecker.java
@@ -0,0 +1,16 @@
+// Checks whether a number is positive, negative, or zero.
+public class NumberSignChecker {
+    public static void main(String[] args) {
+        int x =0;
+        if (x>0){
+            System.out.println("number is positive");
+        }else if (x<0) {
+            System.out.println("number is negative");
+        } else {
+            System.out.println("number is zero");
+        }
+
+    }
+    
+}
+
