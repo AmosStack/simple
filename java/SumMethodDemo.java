c5f2012f8f2ca01bbd3052d4ba07075e9e3c6e86
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-20 12:17:49 +0300

    Create SumMethodDemo.java

---
+++ java/SumMethodDemo.java
@@ -0,0 +1,11 @@
+// Demonstrates a simple method that returns the sum of two numbers.
+public class SumMethodDemo {
+    
+        public static int sum(int x,int y){
+            return x+y;
+            
+        }
+        public static void main(String[] args) {
+            System.out.println(sum(3, 5));
+        }
+    }
